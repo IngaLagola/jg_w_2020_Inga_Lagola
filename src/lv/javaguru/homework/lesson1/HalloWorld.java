368d738eae252774c13d98115cfde53fb131ec70
Inga <inga.lagola@gmail.com>
2020-01-22 21:40:07 +0200

    Yeah, my first program

---
+++ src/lv/javaguru/homework/lesson1/HalloWorld.java
@@ -0,0 +1,8 @@
+package lv.javaguru.homework.lesson1;
+
+public class HalloWorld<svnm, svmn> {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
