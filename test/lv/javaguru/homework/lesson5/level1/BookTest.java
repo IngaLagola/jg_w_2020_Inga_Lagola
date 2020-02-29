718892bea93ca8b47c4b549e81cf1117f05f5aee
Inga <inga.lagola@gmail.com>
2020-02-29 14:21:29 +0200

    lesson5 level1 done

---
+++ test/lv/javaguru/homework/lesson5/level1/BookTest.java
@@ -0,0 +1,105 @@
+package lv.javaguru.homework.lesson5.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest{
+
+    @Test
+    void shouldSetAuthor() {
+        Book book = new Book();
+        book.setAuthor("lulu");
+        assertEquals("lulu", book.getAuthor());
+    }
+
+    @Test
+    void shouldGetAuthor() {
+        Book book = new Book();
+        assertEquals(null, book.getAuthor());
+    }
+
+    @Test
+    void shouldSetTitle() {
+        Book book = new Book();
+        book.setTitle("Emi");
+        assertEquals("Emi", book.getTitle());
+    }
+
+    @Test
+    void shouldGetTitle() {
+        Book book = new Book();
+        assertEquals(null, book.getTitle());
+    }
+
+    @Test
+    void shouldSetPageCount() {
+        Book book = new Book();
+        book.setPageCount(100);
+
+        assertEquals(100, book.getPageCount());
+    }
+
+    @Test
+    void shouldGetPageCount() {
+        Book book = new Book();
+        //   System.out.println(book.getPageCount());
+        assertEquals(0, book.getPageCount());
+    }
+
+    @Test
+    void shouldCheckToString() {
+        Book book = new Book();
+        String author = "lulu";
+        String title = "Emi";
+        int pageCount = 100;
+        String testString = "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", pageCount='" + pageCount + '\'' +
+                '}';
+        book.setAuthor(author);
+        book.setTitle(title);
+        book.setPageCount(pageCount);
+        assertEquals(testString, book.toString());
+    }
+
+    @Test
+    void shouldReturnTrueWhenObjectsAreEqual() {
+        String author = "lulu";
+        String title = "Emi";
+        int pageCount = 100;
+
+        Book firstBook = new Book();
+        Book secondBook = new Book();
+
+        firstBook.setAuthor(author);
+        firstBook.setTitle(title);
+        firstBook.setPageCount(pageCount);
+
+        secondBook.setAuthor(author);
+        secondBook.setTitle(title);
+        secondBook.setPageCount(pageCount);
+
+        assertEquals(true, firstBook.equals(secondBook));
+    }
+    @Test
+    void shouldReturnFalseWhenObjectsAreNotEqual() {
+        String author = "lulu";
+        String title = "Emi";
+        int pageCount = 100;
+
+        Book firstBook = new Book();
+        Book secondBook = new Book();
+
+        firstBook.setAuthor(author);
+        firstBook.setTitle(title);
+        firstBook.setPageCount(pageCount);
+
+        secondBook.setAuthor("kkkk");
+        secondBook.setTitle(title);
+        secondBook.setPageCount(pageCount);
+
+        assertEquals(false, firstBook.equals(secondBook));
+    }
+}
